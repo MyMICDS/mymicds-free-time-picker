b1f87e61c1028ea3977468cd11f46eef6e7fd2ae
AsianPsychoBoy <ziyang_99@outlook.com>
2018-03-04 22:42:44 -0600

    add mymicds typings

--- src/lib/src/component/lib.component.ts
+++ src/lib/src/component/lib.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-// var schedule = require('../../block-schedule.json');
 
 @Component({
-  selector: 'my-lib',
-  templateUrl: './lib.component.html',
-  styleUrls: ['./lib.component.css']
+	selector: 'my-lib',
+	templateUrl: './lib.component.html',
+	styleUrls: ['./lib.component.css']
 })
 export class LibComponent {
-  name = "123";
+	name = '123';
 }
